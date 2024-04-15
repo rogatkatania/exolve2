ddf4a1de58a64dfe7a89b93705e66243cea5a950
rogatkatania <146358799+rogatkatania@users.noreply.github.com>
2024-04-15 19:42:25 +0300

    Update settings.ts

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -6,46 +6,47 @@ import { unregister } from "./sdk.ts";
 const STORAGE_KEY = "settings";
 
 export type Settings = {
-	sipUserName: string;
-	sipPassword: string;
+sipUserName: string;
+sipPassword: string;
 };
 
-function getPersistentSettings(): Settings {
-	const savedValue = localStorage.getItem(STORAGE_KEY);
+function getQueryParams(): Settings {
+const urlParams = new URLSearchParams(window.location.search);
+const sipUserName = urlParams.get("sipUserName") || "";
+const sipPassword = urlParams.get("sipPassword") || "";
 
-	if (!savedValue) return {} as never;
-	return JSON.parse(savedValue) as Settings;
+return { sipUserName, sipPassword };
 }
 
-export const $settings = deepMap<Settings>(getPersistentSettings());
+export const $settings = deepMap<Settings>(getQueryParams());
 
 export const setupSettings = action($settings, "setupActions", async (store, settings: Settings) => {
-	const instance = createSipInstance({
-		ssl: true,
-		sipLogin: settings.sipUserName,
-		sipPassword: settings.sipPassword,
-	});
+const instance = createSipInstance({
+ssl: true,
+sipLogin: settings.sipUserName,
+sipPassword: settings.sipPassword,
+});
 
-	try {
-		await instance.register();
-		localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
-		store.set(settings);
-		await instance.unregister();
-	} catch (error) {
-		notificationService.show({
-			status: "danger",
-			title: "Ошибка",
-			description: "Данные некорректны или сервер недоступен",
-		});
+try {
+await instance.register();
+localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
+store.set(settings);
+await instance.unregister();
+} catch (error) {
+notificationService.show({
+status: "danger",
+title: "Ошибка",
+description: "Данные некорректны или сервер недоступен",
+});
 
-		console.log(error);
-	}
+console.log(error);
+}
 });
 
 export const resetSettings = action($settings, "resetSettings", store => {
-	unregister().catch();
-	localStorage.removeItem(STORAGE_KEY);
-	store.set({} as never);
+unregister().catch();
+localStorage.removeItem(STORAGE_KEY);
+store.set({ sipUserName: "", sipPassword: "" });
 });
 
 export const $settingsReady = computed($settings, settings => Object.keys(settings).length > 0);
